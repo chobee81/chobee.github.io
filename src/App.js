2cba26e24d6e503ddd6d21ca277911b42a0c7c74
chobee <wincbell@naver.com>
2025-02-28 16:56:24 +0900

    deply  명령어 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>리액트 gh-pages 배포하기</h1>
+        <p>npm run deploy로 업데이트</p>
       </header>
     </div>
   );
