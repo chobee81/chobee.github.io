67e8ebe48106e0b0832c26308fb9b5e8061aa9e7
chobee <wincbell@naver.com>
2025-02-28 16:57:26 +0900

    두번째 p

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <h1>리액트 gh-pages 배포하기</h1>
         <p>npm run deploy로 업데이트</p>
+        <p>이후 다시 커밋, 푸쉬</p>
       </header>
     </div>
   );
