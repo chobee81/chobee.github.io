b14d06d49b9c17e004e7d9aec2023d46f7f43786
chobee81 <54051054+chobee81@users.noreply.github.com>
2025-03-05 11:45:41 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <h1>리액트 gh-pages 배포하기</h1>
         <ol>
-          <li>깃허브 레포지토리 생성 후 브랜치 main 변경 후 저장하여 깃페이지 만들기</li>
+          <li>깃허브 레포지토리 생성, 브랜치 main 변경 후 저장하여 깃페이지 만들기</li>
           <li>npm install gh-pages</li>
           <li>npm run build</li>
           <li>package.json 변경
